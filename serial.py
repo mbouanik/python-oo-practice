bc6bcfd6b1574a69c6c188cb8da4def375e439bc
mbouanik <mbouanik@student.42.fr>
2023-08-01 10:38:32 -0500

    add doctest and imp special word finder

--- serial.py
+++ serial.py
@@ -4,6 +4,9 @@ class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
     
     >>> serial = SerialGenerator(start=100)
+    
+    >>> serial 
+    <SerialGenerator start=100 next=101>
 
     >>> serial.generate()
     100
@@ -23,6 +26,9 @@ class SerialGenerator:
         self.start = start
         self.serial_number = start
 
+    def __repr__(self) -> str:
+        return f"<SerialGenerator start={self.start} next={self.start + 1}>"
+    
     def generate(self):
         self.serial_number += 1
         return self.serial_number - 1
--- wordfinder.py
+++ wordfinder.py
@@ -1,8 +1,37 @@
-"""Word Finder: finds random words from a dictionary."""
+"""Word Finder: finds random words from a dictionary.
+    >>> word = WordFinder('words.txt')
+    
+
+    >>> word.random_word() in [word.strip() for word in open(word.file_name, 'r').readlines()]
+    True
+
+    >>> super_word =  SpecialWordFinder('food.txt')
+
+    >>> super_word.special_word() in [word.strip() for word in open(super_word.file_name, 'r').readlines() if word and word[0] != '#']
+    True
+
+"""
+
 
 from random import choice
 
 class WordFinder:
-    def __init__(self):
-        with open('words.txt', 'r') as file:
-           self.rand_word = choice(file.readlines())
+    def __init__(self, file_name):
+        self.file_name = file_name
+    
+    def words_lst(self):
+        with  open(self.file_name, 'r') as file:
+            return [word.strip() for word in file.readlines()]
+    
+    def random_word(self):
+        return choice(self.words_lst())
+
+
+class SpecialWordFinder(WordFinder):
+    def __init__(self, file_name):
+        super().__init__(file_name)
+    
+    def special_word(self):
+        words_lst = super().words_lst()
+        return choice([word for word in words_lst if word and word[0] != '#'])
+
